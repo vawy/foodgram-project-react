18229a24a391dc9748f445fb560b82b2adbce064
Vawy <vasilyvig@gmail.com>
2023-01-30 18:27:26 +0300

    fix some for workflow

--- backend/api/views.py
+++ backend/api/views.py
@@ -66,13 +66,21 @@ class UsersViewSet(UserViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         if request.method == 'DELETE':
-            if subscription.exists():
-                subscription.delete()
-                return Response(status=status.HTTP_204_NO_CONTENT)
-            return Response(
-                {'error': 'Вы не подписаны на этого пользователя'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            if not subscription.exists():
+                return Response(
+                    {'error': 'Вы не подписаны на этого пользователя'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        subscription.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+            # if subscription.exists():
+            #     subscription.delete()
+            #     return Response(status=status.HTTP_204_NO_CONTENT)
+            # return Response(
+            #     {'error': 'Вы не подписаны на этого пользователя'},
+            #     status=status.HTTP_400_BAD_REQUEST
+            # )
 
 
 class TagViewSet(ReadOnlyModelViewSet):
@@ -128,11 +136,17 @@ class RecipeViewSet(ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         if self.request.method == 'DELETE':
-            if model_obj.exists():
-                model_obj.delete()
-                return Response(status=status.HTTP_204_NO_CONTENT)
-            return Response({'error': 'Этого рецепта нет в избранном.'},
-                            status=status.HTTP_400_BAD_REQUEST)
+            if not model_obj.exists():
+                return Response({'error': 'Этого рецепта нет в избранном.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+        model_obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+            # if model_obj.exists():
+            #     model_obj.delete()
+            #     return Response(status=status.HTTP_204_NO_CONTENT)
+            # return Response({'error': 'Этого рецепта нет в избранном.'},
+            #                 status=status.HTTP_400_BAD_REQUEST)
 
     @action(methods=['POST', 'DELETE'], detail=True,
             permission_classes=[IsAuthenticated])
